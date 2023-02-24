bf92719595349d58dbf49bfbf0ba27f95add1e0e
Ramesh Chellaton <ramesh.chellaton@gmail.com>
2023-02-24 17:14:15 -0500

    stage 1

--- file-1.R
+++ file-1.R
@@ -1,7 +1,9 @@
+library(Hmisc)
 titanic <- titanic3
 g <- ggplot(data=titanic, aes(x=pclass, fill=sex)) + 
   ggtitle('Proportion that survived') + 
   labs(x='passenger class', y='survival rate')
-g <- g + geom_col(aes(y=survived)) 
+g <- g + geom_col(position='stack', 
+                  aes(y=length(titanic$survived==1)/length(titanic$survived))) 
   
 print(g)
